c319b9b9f79117df041d3f572046c8d24b947afc
zhangjian <sd4153900@163.com>
2019-03-13 23:31:28 +0800

    4-13 跳转和事件绑定

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,27 @@
 Page({
-  
+  onTap:function(){
+
+    //转发 执行onHide
+    // wx.navigateTo({
+    //   url: '/pages/posts/posts',
+    // })
+    //重定向 执行onUnload
+    wx.redirectTo({
+      url: '/pages/posts/posts',
+    })
+  },
+
+  /**
+  * 生命周期函数--监听页面隐藏
+  */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
 })
