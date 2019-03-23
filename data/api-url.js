f01a9e13e4972671d2b20ec5e9291f64ce4f7d05
zhangjian <sd4153900@163.com>
2019-03-23 22:49:14 +0800

    7-11 电影首页

---
+++ data/api-url.js
@@ -0,0 +1,9 @@
+var urls = {
+  top250: "/v2/movie/top250?start=0&count=3",
+  theaters: "/v2/movie/in_theaters?start=0&count=3",
+  coming: "/v2/movie/coming_soon?start=0&count=3",
+}
+
+module.exports = {
+  urls: urls
+}
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,4 +1,6 @@
 // pages/movies/movies.js
+var apiUrl = require("../../data/api-url.js");
+var app = getApp();
 Page({
 
   /**
@@ -11,58 +13,104 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    // wx.showTabBarRedDot({
-    //   index : 1
-    // });
+  onLoad: function(options) {
+    var urls = apiUrl.urls;
+    var urlList = [urls.theaters, urls.coming, urls.top250];
+    var titleList = ["热映", "即将上映", "top250"];
+    this.getMovieInfo(urlList, titleList, 0, []);
   },
 
+  getMovieInfo: function(urlList, titleList, seq, movieData) {
+    if (!urlList || urlList.length <= 0) {
+      return;
+    }
+    var url = urlList[seq];
+    var title = titleList[seq];
+    var globalData = app.globalData;
+    var that = this;
+    wx.request({
+      url: globalData.doubanBase + url,
+      data: {},
+      method: "GET",
+      success: function(res) {
+        var movieInfo = {};
+        movieInfo.title = title;
+        movieInfo.movies = [];
+        var subjects = res.data.subjects;
+        for (var i = 0; i < subjects.length; i++) {
+          var subject = subjects[i];
+          var movie = {};
+          movie.id = subject.id;
+          movie.title = subject.title;
+          movie.img = subject.images.large;
+          movie.stars = {};
+          movie.stars.score = subject.rating.average;
+          movie.stars.star = subject.rating.stars;
+          movieInfo.movies[i] = movie;
+
+        }
+        movieData[movieData.length] = movieInfo;
+        seq++;
+        if (urlList.length <= seq) {
+          that.setData({
+            movieData
+          });
+        } else {
+          that.getMovieInfo(urlList, titleList, seq, movieData);
+        }
+
+      },
+      fail: function() {
+
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
